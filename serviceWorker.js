83dceea885856d639c220db475d70964cea3f204
Eremenko_Dabila <eryomenkoda@student.bmstu.ru>
2025-12-21 15:51:33 +0300

    Updates

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,28 +1,20 @@
 const CACHE_NAME = "tile-notes-cache-v1";
-const urlsToCache = ["/", "/index.html", "/RepoName/icons/it_soft-192.png", "/RepoName/icons/it_soft-512.png"];
+const urlsToCache = ["/RepoName/", "/RepoName/index.html", "/RepoName/icons/it_soft-192.png", "/RepoName/icons/it_soft-512.png"];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) return caches.delete(cache);
-        })
-      )
+      Promise.all(cacheNames.map((cache) => (cache !== CACHE_NAME ? caches.delete(cache) : null)))
     )
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
 });
