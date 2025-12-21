9a55da52db82b192cb17a912fab1057fea344431
Eremenko_Dabila <eryomenkoda@student.bmstu.ru>
2025-12-21 15:44:11 +0300

    Updates

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,13 +1,28 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installing...');
-  self.skipWaiting(); // активируем сразу
+const CACHE_NAME = "tile-notes-cache-v1";
+const urlsToCache = ["/", "/index.html", "/RepoName/icons/it_soft-192.png", "/RepoName/icons/it_soft-512.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker activated');
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) return caches.delete(cache);
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-  console.log('Fetching:', event.request.url);
-  // Здесь можно добавить кеширование
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
 });
