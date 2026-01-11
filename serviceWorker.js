ac605558d0cc0a70c94077eac0e0d9f1d8f7c4aa
Eremenko_Dabila <eryomenkoda@student.bmstu.ru>
2026-01-11 18:21:17 +0300

    Updates

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,26 +1,26 @@
-const CACHE_NAME = "sotfware-app-cache-v1";
+const CACHE_NAME = "software-app-cache-v1";
 const urlsToCache = [
   "/sotfware_app/",
   "/sotfware_app/index.html",
-  "/sotfware_app/icons/it_soft.png"
+  "/sotfware_app/icons/it_soft.png",
+  "/sotfware_app/icons/Git_icon.png",
+  "/sotfware_app/icons/Postgre_SQL.png",
+  "/sotfware_app/icons/dbeaver.png",
+  "/sotfware_app/icons/search.png",
+  "/sotfware_app/icons/cart.png"
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cache) => (cache !== CACHE_NAME ? caches.delete(cache) : null))
-      )
-    )
-  );
-  self.clients.claim();
-});
-
 self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
